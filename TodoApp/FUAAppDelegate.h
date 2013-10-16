46692301622dd7608a8b793c4d7c7ea7820972a8
Therin Irwin <therin.irwin@gmail.com>
2013-10-15 20:00:55 -0700

    Initial Commit

---
+++ TodoApp/FUAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  FUAAppDelegate.h
+//  TodoApp
+//
+//  Created by Therin Irwin on 10/15/13.
+//  Copyright (c) 2013 Therin Irwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FUAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TodoApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TodoApp
+//
+//  Created by Therin Irwin on 10/15/13.
+//  Copyright (c) 2013 Therin Irwin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FUAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FUAAppDelegate class]));
+    }
+}
